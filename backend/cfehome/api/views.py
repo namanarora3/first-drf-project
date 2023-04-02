03f3eea7cd81958210e802810c8113e0facf9643
Naman <naman30103@gmail.com>
2023-04-03 03:21:49 +0530

    initial setup

---
+++ backend/cfehome/api/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+import json
+
+def api_home(request,*args,**kwargs):
+    # request -> http request from django
+    # sent json request can be found in request.body
+
+    body = request.body  #we receive a btye string od JSON data
+    resp = {}
+    try:
+        resp = json.loads(body) #byte string of json data -> python dict
+    except:
+        pass
+
+    resp['params'] = dict(request.GET)
+    resp['headers'] = dict(request.headers)
+    resp['content_type'] = request.content_type
+    return JsonResponse(resp)
