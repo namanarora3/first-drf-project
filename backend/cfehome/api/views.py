59012fabacbcb38b47e99a54e5d5fac61c10c0d0
Naman <naman30103@gmail.com>
2023-04-03 13:21:11 +0530

    8 - django model instance as API resource

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,18 +1,33 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 
-def api_home(request,*args,**kwargs):
-    # request -> http request from django
-    # sent json request can be found in request.body
+# echo get data
+def api_home_old1(request,*args,**kwargs):
+#     # request -> http request from django
+#     # sent json request can be found in request.body
 
-    body = request.body  #we receive a btye string od JSON data and thats it
+#     body = request.body  #we receive a btye string od JSON data and thats it
     resp = {}
-    try:
-        resp = json.loads(body) #byte string of json data -> python dict
-    except:
-        pass
+#     try:
+#         resp = json.loads(body) #byte string of json data -> python dict
+#     except:
+#         pass
 
-    resp['params'] = dict(request.GET)
-    resp['headers'] = dict(request.headers) #httpheaders is not JSON serialable, thats why we use dict
-    resp['content_type'] = request.content_type
+#     resp['params'] = dict(request.GET)
+#     resp['headers'] = dict(request.headers) #httpheaders is not JSON serialable, thats why we use dict
+#     resp['content_type'] = request.content_type
     return JsonResponse(resp)
+
+#django model instance as API resource
+def api_home(request,*args,**kwargs):
+    model_data =   Product.objects.all().order_by('?').first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        #here we are converting a model instance(model_data) into a python dict(data) which becomes jsonresponse
+        #this is also known as serialization
+    return JsonResponse(data)
